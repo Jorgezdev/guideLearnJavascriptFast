11938c8bde0c2f707981c1ac74df4af32f5193b5
Jorge Zavala <kokeaft@gmail.com>
2026-02-17 12:40:59 -0300

    translate list objects and date, pending Date

--- list-objects-and-date.js
+++ list-objects-and-date.js
@@ -3,11 +3,11 @@ const lista = [1, "hola", true, null, undefined, { nombre: "Juan", edad: 30 }, [
 ];
 const lista2 = new Array(1, "hola", true, null, undefined);
 
-const lista3 = new Array(5); // Crea un array con 5 elementos vacíos
+const lista3 = new Array(5); // create an array with 5 empty elements 
 lista3[0] = "primer elemento, index 0 de la lista3";
 
 
-const lista4 = [lista, lista2, lista3]; // Lista de listas
+const lista4 = [lista, lista2, lista3]; // list of lists
 
 console.log("Lista 1:");
 console.log("Lista 2:");
@@ -16,7 +16,7 @@ console.log("Lista 4:");
 
 
 // objects 
-// definimos const como persona, pero no su interior, por lo que podemos modificar sus propiedades, pero no reasignar la variable persona a otro objeto
+// We define const as person, but not its contents, so we can modify its properties, but not reassing the person variable to another object 
 const persona = {
     nombre: "Juan",
     edad: 30,
@@ -27,8 +27,8 @@ const persona = {
     }
 };
 
-console.log(persona.profesion); // Accediendo a la propiedad "profesion" del objeto persona
+console.log(persona.profesion); // Accessing the "profession" property of the person object
 
 
 // Date
-new Date(); // Crea un nuevo objeto de fecha con la fecha y hora actuales, siempre mayuscula D en Date
+new Date(); // Create a new date object with the current date and time, always using uppercase D in Date.
