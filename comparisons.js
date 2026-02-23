a4568d23a6145fa0fed07c4254ce5e7a051653e5
Jorge Zavala <kokeaft@gmail.com>
2026-02-23 17:13:47 -0300

    comparison

--- comparisons.js
+++ comparisons.js
@@ -1,7 +1,7 @@
-// Los comparadores son operadores que se utilizan para comparar dos valores y devolver un resultado booleano (verdadero o falso) dependiendo de si la comparación es cierta o no. Aquí hay algunos de los comparadores más comunes en JavaScript:
+// Comparators are operators used to compare two values and return a Boolean result (true or false) depending on whether the comparison is true or false. Here are some common comparators in Javascript:
 
+// Equality
 
-// igualdad 
 if (5 == 5) {
     console.log("5 es igual a 5 ");
 }
@@ -15,32 +15,53 @@ console.log(typeof a); // number
 let b = "5";
 console.log(typeof b); // string
 
-// == solo compara el valor
-// === compara el valor y el tipo de dato
+// == just compare the value
+// === compares the value and the data type
 
 if (a == b ) {
-    console.log("a es igual a b");
+    console.log("a is equal to b");
 }
 
 if (a === b) {
-    console.log("a es muy igual a b");       
+    console.log("a is very equal to b");       
 }else {
-    console.log("a no es muy igual a b");
+    console.log("a is not very equal to b");
  }
 
 
- // desigualdad
+ // inequality, before putting the exclamation mark to negate equality.
 
  let c = 8;
  let f = "8";
 
  if (c != f) {
-    console.log(c + " es diferente a " + f)
+    console.log(c + " is different from " + f)
  } 
 
 
  if (c !== f) {
-    console.log(c + " es muy diferente a " + f)
+    console.log(c + " is very different from " + f)
  }
+
+ // Comparisons of greater than and less than...
+
+
+let x = 10;
+let y = 20;
+
+if ( x >= y){
+    console.log("x is greater than or equal to y");
+}else { console.log("x is less than y");
+}
+
+let m = 15;
+let n = 10;
+
+if (m < n) {
+    console.log("m is less than n");
+}else {
+    console.log("m is greater than or equal to n");
+}
+
 
  
