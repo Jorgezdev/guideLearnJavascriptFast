0833ca4518ba65ef68f6ebae6e3212067051f343
Jorge Zavala <kokeaft@gmail.com>
2026-02-24 17:29:22 -0300

    loops

---
+++ loopsForAndWhile06.js
@@ -0,0 +1,15 @@
+// Loops For
+// inicializacion; condicion ; actualizacion
+
+
+// terminos generales que cumplen la misma funcion:
+/*
+i = i + 1
+i += 1
+i++
+*/
+
+for (let i = 0; i < 80; i = i + 8) {
+    console.log(i);
+}
+
