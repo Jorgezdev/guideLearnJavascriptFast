904872f6a1ec1b79632def444095af7503ffc267
Jorge Zavala <kokeaft@gmail.com>
2026-02-26 17:33:37 -0300

    bucles for types

--- loopsForAndWhile06.js
+++ loopsForAndWhile06.js
@@ -8,8 +8,18 @@ i = i + 1
 i += 1
 i++
 */
-
+/*
 for (let i = 0; i < 80; i = i + 8) {
     console.log(i);
 }
+
+
+let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ */
+// i dentro de corchetes queremos que nos imprima el valor dentro de la posicion, sin corchetes nos imprime la posicion (lista[i]) (i)
+let lista = [1, 3, 4, 6, 7, 8, 10];
+for (let i = 0; i < lista.length; i++) {
+    console.log(lista[i]);
+}
+
 
