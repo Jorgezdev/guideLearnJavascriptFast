f0f1971a02b6872001b3793e25a603c54feb6f09
Jorge Zavala <kokeaft@gmail.com>
2026-03-04 16:34:12 -0300

    while and do while

--- loopsForAndWhile06.js
+++ loopsForAndWhile06.js
@@ -51,3 +51,26 @@ for (let atributo in persona) {
     console.log(atributo);
     console.log(persona[atributo]);
 }
+
+
+
+// Loops While
+// existen dos tipos de loops while: while y do while
+// while
+let i = 0;  
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// do while
+let j = 0;
+do {
+    console.log(j + " este console log imprime do while" );
+    j++;
+} while (j < 5)
+
+
+
+// do while se usa mucho menos que while, pero es importante conocerlo porque se ejecuta al menos una vez, incluso si la condicion es falsa, mientras que while no se ejecuta si la condicion es falsa desde el principio.. prueba cambiando el valor de j a uno mayor que el de la condicion (un numero mayor a >5)
+// y te imprimira el console log al menos una vez, aunque la condicion sea falsa desde el principio.
