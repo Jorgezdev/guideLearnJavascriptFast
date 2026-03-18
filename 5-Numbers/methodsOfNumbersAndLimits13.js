e227ecf562b1f2ab4363d8b2ad7a580b552ea312
Jorge Zavala <kokeaft@gmail.com>
2026-03-18 17:26:57 -0300

    methods

---
+++ 5-Numbers/methodsOfNumbersAndLimits13.js
@@ -0,0 +1,16 @@
+let numeroA = 4;
+
+let numeroB = Number(5);
+
+
+
+
+
+let numeroC = 19;
+
+let numeroD = 0;
+
+let division = (numeroC / numeroD); // el valor infinity puede darte cuando intentes dividir un valor con 0 o con un valor null.
+ 
+console.log(division);
+
