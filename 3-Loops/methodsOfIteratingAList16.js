bc62efbbae825fc56111f4195bbdbb2d51cfec6d
Jorge Zavala <kokeaft@gmail.com>
2026-04-02 17:33:21 -0300

    iterating a list

---
+++ 3-Loops/methodsOfIteratingAList16.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4, 5, true, NaN, undefined, null, 'hola'];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}// con el bucle for se puede iterar sobre una lista y obtener cada uno de sus elementos, pero no es la forma mas eficiente de hacerlo, ya que se tiene que acceder a cada elemento por su indice, lo que puede ser lento si la lista es muy grande
+
+console.log(array); // imprime la array completa, pero no es lo mismo que iterar sobre cada elemento de la array, ya que no se puede acceder a cada elemento individualmente
+
+
+// Forma ES6 de iterar sobre una lista, con el metodo .forEach() que recibe una funcion como argumento y ejecuta esa funcion para cada elemento de la lista, pasando el elemento como argumento a la funcion
+
+array.forEach(element => console.log(element)); // con el metodo .forEach() se puede iterar sobre una lista y obtener cada uno de sus elementos de forma mas eficiente que con el bucle for, ya que no se tiene que acceder a cada elemento por su indice, sino que se obtiene directamente el elemento en cada iteracion, lo que es mas rapido si la lista es muy grande
+
+// tambien se puede usar el metodo .map() que recibe una funcion como argumento y ejecuta esa funcion para cada elemento de la lista, pasando el elemento como argumento a la funcion, pero a diferencia de .forEach() devuelve una nueva lista con los resultados de la funcion aplicada a cada elemento de la lista original
